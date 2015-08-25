54684b381a367da15d2ffce5e3e2489f0e5f5d18
Saqib Omer <TOxIC@macbookairs-MacBook-Air.local>
2015-08-25 17:16:16 +0500

    Created Project

--- GigglyCamera/GigglyCameraViewController.swift
+++ GigglyCamera/GigglyCameraViewController.swift
@@ -8,11 +8,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class GigglyCameraViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        println("Some Text")
     }
 
     override func didReceiveMemoryWarning() {
