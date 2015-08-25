60275d1f139ddf9d95f2a16d0c84f7a104bc8b64
Saqib Omer <TOxIC@macbookairs-MacBook-Air.local>
2015-08-25 17:13:38 +0500

    Initial Commit

---
+++ GigglyCamera/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GigglyCamera
+//
+//  Created by Saqib Omer on 8/25/15.
+//  Copyright (c) 2015 Kaboom Labs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
